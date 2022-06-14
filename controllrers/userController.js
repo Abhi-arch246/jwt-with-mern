a2125b515d8f874952c88f68d7f64a5fbf941078
Abhi-arch246 <abhishekreddy235@gmail.com>
2022-06-15 02:17:12 +0530

    Frontend pages created

--- controllrers/userController.js
+++ controllrers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/userModel')
-
+const bcrypt = require('bcryptjs')
 const registerUser = async (req, res) => {
 
     const { user, email, password } = req.body
@@ -8,10 +8,12 @@ const registerUser = async (req, res) => {
         return res.status(200).json({ msg: "User already regisetred with this email" })
     } else {
         try {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password, salt)
             const data = await User.create({
                 user: user,
                 email: email,
-                password: password
+                password: hashedPassword
             })
 
             return res.status(200).json(data)
@@ -27,7 +29,7 @@ const loginUser = async (req, res) => {
     const { email, password } = req.body
     const user = await User.findOne({ email })
     if (user) {
-        if (password === user.password) {
+        if (user && (await bcrypt.compare(password, user.password))) {
             return res.status(200).json({ msg: "Successful login" })
         }
         else {
