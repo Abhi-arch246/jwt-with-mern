11f0ce753817a783444dd92286dad93e676a17b1
Abhi-arch246 <abhishekreddy235@gmail.com>
2022-08-29 20:47:02 +0530

    deploy edits

--- server.js
+++ server.js
@@ -12,4 +12,12 @@ app.use('/api/auth/', authRoute)
 app.use('/api/user/', userRoute)
 
 
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    })
+}
+
+
 app.listen(port, () => console.log(`Server running on ${port}`))
